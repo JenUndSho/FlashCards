f47781ddc0f899e9575465e5bf688e9b0c8128c2
JenUndSho <evgeniyyy.evgeniyyy@gmail.com>
2022-03-02 18:33:31 +0200

    msg

---
+++ Flashcards/task/src/flashcards/Constants.java
@@ -0,0 +1,8 @@
+package flashcards;
+
+public class Constants {
+    public static final String MENU = "Input the action (add, remove, import, export, ask, exit):";
+    public static final String BYE = "Bye bye!";
+    public static final String THE_CARD = "The card:";
+    public static final String FILE_NAME = "File name:";
+}
